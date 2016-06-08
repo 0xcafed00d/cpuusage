b476cc4ca27c2dd08ad72543752fdfab43cd232e
Lee Witek <neomech@gmail.com>
2016-06-08 15:49:26 +0100

    docs

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # cpuusage [![GoDoc](https://godoc.org/github.com/simulatedsimian/cpuusage?status.svg)](https://godoc.org/github.com/simulatedsimian/cpuusage)
 
 Go library that monitors real time CPU usage (total and per-core) (linux only)
+
+## Installation:
+```bash
+$ go get github.com/simulatedsimian/cpuusage
+```
+
+## Example:
+```go
+import "github.com/simulatedsimian/cpuusage"
+```
+```go
+   // display the cpu usage once per second
+u := cpuusage.Usage{}
+
+for {
+  err := u.Measure()
+  if err != nil {
+    // handle error....
+  }
+  fmt.Printf("Overall %: %d Per Core %: %v\n", u.Overall, u.Cores)
+  time.Sleep(1 * time.Second)
+}```
--- api.go
+++ api.go
@@ -1,4 +1,20 @@
 // Package cpuusage library that monitors real time CPU usage (total and per-core) (linux only)
+//
+// Installation:
+//   go get github.com/simulatedsimian/cpuusage
+//
+// Example:
+//   // display the cpu usage once per second
+//   u := cpuusage.Usage{}
+//
+//   for {
+//       err := u.Measure()
+//       if err != nil {
+//           // handle error....
+//       }
+//       fmt.Printf("Overall %: %d Per Core %: %v\n", u.Overall, u.Cores)
+//       time.Sleep(1 * time.Second)
+//  }
 package cpuusage
 
 // Usage struct holds the overall cpu usage and the per-core usage resulting from the last call to Measure().
@@ -6,12 +22,14 @@ package cpuusage
 type Usage struct {
 	// Overall is the combined cpu usage of all cores in the system
 	Overall int
-
+	// Cores is a slice of ints holding the usage of each core (physical or virtual) in the system
 	Cores    []int
 	previous *cpuinfo
 }
 
 // Measure reads the /proc/stat file and extracts the cpu usage information, writing it into the Usage struct.
+// The first call to Measure gives the usage since the system was booted, with subsequent calls giving the
+// usage during the period of time from the previous call to the current one.
 func (u *Usage) Measure() error {
 	cpu, err := readProcStat()
 	if err != nil {
